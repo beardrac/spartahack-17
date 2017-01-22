2ee377178e6b56dacce1cd84b34608e22b9d9f84
Rachel Beard <Rachel@Rachels-MacBook-Air.local>
2017-01-21 19:49:38 -0500

    added parsers

---
+++ parsing-step-2.py
@@ -0,0 +1,31 @@
+# Parsing congress.gov/members/
+
+import requests
+from html.parser import HTMLParser
+from lxml import html
+from lxml.html import fromstring
+
+# FIRST: Get Senators and Reps's websites.
+
+# create dictionary
+takeadict = []
+
+# get info from website
+page = requests.get('https://www.congress.gov/members?pageSize=250&q={"congress":"115"}')
+tree = html.fromstring(page.content)
+
+# get senator urls
+hrefs = tree.xpath('//select[@id="members-senators"]/option/@value')
+del hrefs[0]
+print(len(hrefs))
+
+# get representative urls
+representativehrefs = tree.xpath('//select[@id="members-representatives"]/option/@value')
+del representativehrefs[0]
+print(len(representativehrefs))
+
+# put senators in dictionary
+for i in range(441):
+    hrefs.append( representativehrefs[ i ] )
+
+#SECOND: Yo visit all those links tho. And get all the data.
